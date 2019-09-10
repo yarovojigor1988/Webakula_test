bf8ba6ec463cdb72f7f5d46bd57f2eaea64ed1a3
yarovojigor1988 <yarovojigor1988@gmail.com>
2019-09-10 23:20:42 +0300

    slider

---
+++ js/app.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.carousel').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 20000,
+        dots: true,
+        аccessibility: false
+    });
+  });
